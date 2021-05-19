1daa53a2e0d8e72eea92bc9a07dcbba2cb0367e1
Sami Khan <76680018+samikhan805@users.noreply.github.com>
2021-05-19 23:23:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Print = document.querySelector('.print')
 var serial_notifier = document.querySelector('.serial_text')
 var total_serial_number = document.querySelector('.serial_text_number')
 
-alert('Welcome to the Random Serial Number Generating System developed by ' + name.toUpperCase() + '. \nTo use this system, type your desired number into the \'Input\' section. Then click on the \'Generate\' to get the result. You can also get it by pressing ENTER key. \n\nNote: Make sure that you\'ve at least a single digit in your \'Input\'. Otherwise, you\'ll get an error. Why so late, dear? Give it a try.')
+alert('Welcome to the Random Serial Number Generating System developed by ' + name.toUpperCase() + '. \nTo use this system, type your desired number into the \'Input\' section. Then click on the \'Generate\' to get the result. You can also get it by pressing ENTER key. \n\nNote: Make sure that you\'ve at least a single digit in your \'Input\'. And your input value must be getter than zero (0). Otherwise, you\'ll get an error. Why so late, dear? Give it a try.')
 
 function mainProgram() {
   for (var i = 1; i <= input.value; i++) {
